ef04899c2f997a6002b7a5dc44ea9dc23a7dc811
DESKTOP-7A40RVD\Anton <selmernoid@mail.ru>
2019-10-24 01:49:46 +0300

    use Foundatio simple message.

---
+++ src/NewsUpdater/MessagingModels/Message_RedditPostUpdate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsUpdater.MessagingModels
+{
+    public class Message_RedditPostUpdate
+    {
+        public string Link { get; set; }
+    }
+}
--- src/NewsUpdater/Program.cs
+++ src/NewsUpdater/Program.cs
@@ -4,12 +4,16 @@ using Microsoft.Extensions.Configuration.Json;
 using NewsUpdater.DataFlowControllers;
 using System;
 using System.IO;
+using Foundatio.Messaging;
+using NewsUpdater.MessagingModels;
+using RedditSharp.Things
+using System.Collections.Generic;
 
 namespace NewsUpdater
 {
     class Program
     {
-        static void Main(string[] args)
+        static async System.Threading.Tasks.Task Main(string[] args)
         {
             IConfiguration builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
@@ -17,17 +21,33 @@ namespace NewsUpdater
                 .AddJsonFile("appsettings.private.json", optional: true, reloadOnChange: true)
                 .Build();
 
+
+            IMessageBus messageBus = new InMemoryMessageBus();
+            //IMessageBus messageRedisBus = new RedisMessageBus();
+
             var a = new DataGrabberReddit(builder);
 
             //TODO: queued refresh data
             //TODO: store to db values (docker)
             //TODO: take grafana online (docker)
 
-            a.UpdatePostInfo("/r/Pikabu/comments/dl6ngi/книги/");
-            a.UpdatePostInfo("https://www.reddit.com/r/science/comments/dj4z87/from_2007_to_2017_the_number_of_suicides_among/");
+            var listToUpdate = new List<string>();
+            listToUpdate.Add("/r/Pikabu/comments/dl6ngi/книги/");
+            listToUpdate.Add("/r/science/comments/dj4z87/from_2007_to_2017_the_number_of_suicides_among/");
 
-            a.ReadPosts("pikabu");
-            a.ReadPosts("WTF");
+            //a.ReadPosts("pikabu");
+            //a.ReadPosts("WTF");
+
+            await messageBus.SubscribeAsync<Message_RedditPostUpdate>(msg => {
+                Console.WriteLine($"Now we  take: {msg.Link}");
+                // Got message
+                a.UpdatePostInfo(msg.Link);
+            });
+
+            foreach (var postUrl in listToUpdate)
+            {
+                await messageBus.PublishAsync(new Message_RedditPostUpdate { Link = postUrl });
+            }
 
 
             Console.WriteLine("");
