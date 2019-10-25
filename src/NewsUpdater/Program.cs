50f846a90ca48055c17df4b83de24126a14cb925
DESKTOP-7A40RVD\Anton <selmernoid@mail.ru>
2019-10-26 02:45:52 +0300

    redis test

--- src/NewsUpdater/Program.cs
+++ src/NewsUpdater/Program.cs
@@ -6,8 +6,10 @@ using System;
 using System.IO;
 using Foundatio.Messaging;
 using NewsUpdater.MessagingModels;
-using RedditSharp.Things
+using RedditSharp.Things;
 using System.Collections.Generic;
+using StackExchange.Redis;
+using Foundatio.Caching;
 
 namespace NewsUpdater
 {
@@ -21,38 +23,48 @@ namespace NewsUpdater
                 .AddJsonFile("appsettings.private.json", optional: true, reloadOnChange: true)
                 .Build();
 
-
-            IMessageBus messageBus = new InMemoryMessageBus();
-            //IMessageBus messageRedisBus = new RedisMessageBus();
-
-            var a = new DataGrabberReddit(builder);
-
-            //TODO: queued refresh data
-            //TODO: store to db values (docker)
-            //TODO: take grafana online (docker)
-
-            var listToUpdate = new List<string>();
-            listToUpdate.Add("/r/Pikabu/comments/dl6ngi/книги/");
-            listToUpdate.Add("/r/science/comments/dj4z87/from_2007_to_2017_the_number_of_suicides_among/");
-
-            //a.ReadPosts("pikabu");
-            //a.ReadPosts("WTF");
-
-            await messageBus.SubscribeAsync<Message_RedditPostUpdate>(msg => {
-                Console.WriteLine($"Now we  take: {msg.Link}");
-                // Got message
-                a.UpdatePostInfo(msg.Link);
-            });
-
-            foreach (var postUrl in listToUpdate)
+            //RedisHybridCacheClient:
+            //IMessageBus messageBus = new InMemoryMessageBus();
+            //messageBus = new RedisMessageBus(new RedisMessageBusOptions { });
+            //IMessageBus messageRedisBus = new RedisMessageBus(new RedisMessageBusOptions { });
+            var log = new StringWriter();
+            using (var muxer = ConnectionMultiplexer.Connect("localhost:63790", log))
             {
-                await messageBus.PublishAsync(new Message_RedditPostUpdate { Link = postUrl });
+                var _redisCache = new RedisCacheClient(new RedisCacheClientOptions { ConnectionMultiplexer = muxer });
+                _redisCache.RemoveAllAsync().GetAwaiter().GetResult();
+                IMessageBus messageBus = new RedisMessageBus(new RedisMessageBusOptions { Subscriber = muxer.GetSubscriber(), Topic = "test-cache" });
+                var _hybridCacheClient = new HybridCacheClient(_redisCache, messageBus);
+
+                var a = new DataGrabberReddit(builder);
+
+                //75% done: queued refresh data
+                //60% done: store to db values (docker)
+                //TODO: take grafana online (docker)
+
+                var listToUpdate = new List<string>();
+                listToUpdate.Add("/r/Pikabu/comments/dl6ngi/книги/");
+                listToUpdate.Add("/r/science/comments/dj4z87/from_2007_to_2017_the_number_of_suicides_among/");
+
+                //a.ReadPosts("pikabu");
+                //a.ReadPosts("WTF");
+
+                await messageBus.SubscribeAsync<Message_RedditPostUpdate>(msg =>
+                {
+                    Console.WriteLine($"Now we  take: {msg.Link}");
+                    // Got message
+                    a.UpdatePostInfo(msg.Link);
+                });
+
+                foreach (var postUrl in listToUpdate)
+                {
+                    await messageBus.PublishAsync(new Message_RedditPostUpdate { Link = postUrl });
+                }
+
+
+                Console.WriteLine("");
+                Console.WriteLine("FINISH");
+                Console.ReadKey();
             }
-
-
-            Console.WriteLine("");
-            Console.WriteLine("FINISH");
-            Console.ReadKey();
         }
     }
 }
