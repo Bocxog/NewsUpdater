5e51124d18bdae4347128faba83b6e02474e262a
DESKTOP-7A40RVD\Anton <selmernoid@mail.ru>
2019-11-08 01:37:15 +0200

    remove foundatio

--- src/NewsUpdater/Program.cs
+++ src/NewsUpdater/Program.cs
@@ -4,12 +4,9 @@ using Microsoft.Extensions.Configuration.Json;
 using NewsUpdater.DataFlowControllers;
 using System;
 using System.IO;
-using Foundatio.Messaging;
 using NewsUpdater.MessagingModels;
 using RedditSharp.Things;
 using System.Collections.Generic;
-using StackExchange.Redis;
-using Foundatio.Caching;
 using ScheduleLib;
 using System.Threading.Tasks;
 using System.Threading;
@@ -27,19 +24,9 @@ namespace NewsUpdater
                 .AddJsonFile("appsettings.private.json", optional: true, reloadOnChange: true)
                 .Build();
 
-            //RedisHybridCacheClient:
-            //IMessageBus messageBus = new InMemoryMessageBus();
-            //messageBus = new RedisMessageBus(new RedisMessageBusOptions { });
-            //IMessageBus messageRedisBus = new RedisMessageBus(new RedisMessageBusOptions { });
             var log = new StringWriter();
             using (var scheduler = new Scheduler())
-            using (var muxer = ConnectionMultiplexer.Connect("localhost:63790", log))
             {
-                var _redisCache = new RedisCacheClient(new RedisCacheClientOptions { ConnectionMultiplexer = muxer });
-                _redisCache.RemoveAllAsync().GetAwaiter().GetResult();
-                IMessageBus messageBus = new RedisMessageBus(new RedisMessageBusOptions { Subscriber = muxer.GetSubscriber(), Topic = "test-cache" });
-                var _hybridCacheClient = new HybridCacheClient(_redisCache, messageBus);
-
                 dataGrabber = new DataGrabberReddit(builder);
 
                 //75% done: queued refresh data
@@ -53,18 +40,6 @@ namespace NewsUpdater
                 //dataGrabber.ReadPosts("pikabu");
                 //a.ReadPosts("WTF");
                 await SetQueueToUpdateSubredditInfo(scheduler);
-                //await messageBus.SubscribeAsync<Message_RedditPostUpdate>(msg =>
-                //{
-                //    Console.WriteLine($"Now we  take: {msg.Link}");
-                //    // Got message
-                //    dataGrabber.UpdatePostInfo(msg.Link);
-                //});
-
-                //foreach (var postUrl in listToUpdate)
-                //{
-                //    await messageBus.PublishAsync(new Message_RedditPostUpdate { Link = postUrl });
-                //}
-
 
                 Console.WriteLine("");
                 Console.WriteLine("FINISH");
